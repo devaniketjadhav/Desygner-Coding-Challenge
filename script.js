004a0fc23c29bfd58435b44fe508be36f92d8813
Aniket Jadhav <devaniketjadhav@gmail.com>
2020-12-21 09:21:09 +1100

    commenting on root file

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ class TempTracker {
         this.temp = [];
     }
 
+    //method to trigger when new temperature is supplied.
+    //It will add the temp in array and call methods to update min, max and avg.
     insertTemperature = (newTemp) => {
         this.count++;
         this.temp.push(newTemp);
@@ -15,18 +17,21 @@ class TempTracker {
         this.avgTemp(newTemp);
     }
 
+    //method to set max temperature
     maxTemp(newTemp){
         if(this.max < newTemp){
             this.max = newTemp;
         }
     }
 
+    //method to set min temperature
     minTemp(newTemp){
         if(this.min > newTemp){
             this.min = newTemp;
         }
     }
 
+    //method to set new temperature
     avgTemp(newTemp){
         this.avg = (this.avg*(this.count-1)+newTemp)/this.count;
         console.log(`Max: ${this.max}, Min: ${this.min}, Avg: ${this.avg}`);
@@ -34,6 +39,8 @@ class TempTracker {
 }
 
 const updateTemp = new TempTracker();
+
+//suppplying new temperature between 0 to 100 to TempTracker class every 1000ms using setInterval.
 setInterval(function(){
     var newTemp = 100 * Math.random();
     updateTemp.insertTemperature(newTemp);
